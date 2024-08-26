5a2a32788c171f56f7b2217587ddc1bc330951b2
vyacheslavi <vyach.karepov@mail.ru>
2024-08-26 16:16:22 +0700

    add NewsViewSet

--- forum/api/views.py
+++ forum/api/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework import mixins
+from rest_framework.viewsets import GenericViewSet
 
-# Create your views here.
+from .models import News
+from .serializers import NewsSerializer
+
+
+class NewsViewSet(
+    mixins.DestroyModelMixin,
+    mixins.RetrieveModelMixin,
+    mixins.ListModelMixin,
+    mixins.CreateModelMixin,
+    mixins.UpdateModelMixin,
+    GenericViewSet,
+):
+
+    model = News
+    queryset = News.objects.all()
+    serializer_class = NewsSerializer
