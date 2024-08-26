8bb00585ef6cef9a8fe140b8acce7a550636adff
vyacheslavi <vyach.karepov@mail.ru>
2024-08-26 16:15:54 +0700

    add api/v1/ path to urls of application

--- forum/forum/urls.py
+++ forum/forum/urls.py
@@ -24,4 +24,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("summernote/", include("django_summernote.urls")),
+    path("api/v1/", include("api.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
