f8a06e53774d40bc216dc0b9487be660b1cfc0d7
vyacheslavi <vyach.karepov@mail.ru>
2024-08-26 08:48:28 +0700

    add admin summernote model

--- forum/api/admin.py
+++ forum/api/admin.py
@@ -1,3 +1,22 @@
+from django.utils.safestring import mark_safe
 from django.contrib import admin
+from django_summernote.admin import SummernoteModelAdmin
+from api.models import News
 
-# Register your models here.
+
+@admin.register(News)
+class News(SummernoteModelAdmin):
+    summernote_fields = ("text",)
+    list_display = (
+        "title",
+        "image",
+        "text",
+        "created_at",
+        "author",
+    )
+    search_fields = ("title", "text")
+    ordering = ("-created_at",)
+    readonly_fields = ["preview_image"]
+
+    def preview_image(self, obj):
+        return mark_safe(f'<img src="{obj.image.url}" style="max-height: 200px;">')
