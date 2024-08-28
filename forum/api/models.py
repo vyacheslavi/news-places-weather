596a946d27406b38792121d98f395d357ae8e4c5
vyacheslavi <vyach.karepov@mail.ru>
2024-08-28 10:16:43 +0700

    init Place model

--- forum/api/models.py
+++ forum/api/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.core.validators import MaxValueValidator, MinValueValidator
+
+from django_admin_geomap import GeoItem
 
 
 class News(models.Model):
@@ -27,3 +30,33 @@ class News(models.Model):
 
     def __unicode__(self):
         return str(self.about_desc)
+
+
+class Place(models.Model, GeoItem):
+    name = models.CharField(
+        max_length=50,
+        verbose_name="Название места",
+        unique=True,
+    )
+    lon = models.FloatField(verbose_name="Долгота")
+    lat = models.FloatField(verbose_name="Широта")
+    rating = models.IntegerField(
+        validators=[
+            MaxValueValidator(25),
+            MinValueValidator(0),
+        ],
+        default=0,
+        verbose_name="Рейтинг",
+    )
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
+
+    class Meta:
+        ordering = ("-created_at",)
+
+    @property
+    def geomap_longitude(self):
+        return str(self.lon)
+
+    @property
+    def geomap_latitude(self):
+        return str(self.lat)
