d418ba85c5a5ec44c138fa7c7a664be9def1fac7
vyacheslavi <vyach.karepov@mail.ru>
2024-08-29 10:00:51 +0700

    add weather serializer

--- forum/api/serializers.py
+++ forum/api/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from .models import News
+from .models import News, Weather
 
 
 class NewsSerializer(serializers.ModelSerializer):
@@ -14,4 +14,19 @@ class NewsSerializer(serializers.ModelSerializer):
             "image",
             "text",
             "author",
+        ]
+
+
+class WeatherSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Weather
+        fields = [
+            "temperature",
+            "humidity",
+            "pressure",
+            "wind_direction",
+            "wind_speed",
+            "place",
+            "date",
         ]
