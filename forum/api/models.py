d69564cdbc3f8a1e5fdad3811b97ccac9beba7de
vyacheslavi <vyach.karepov@mail.ru>
2024-08-29 10:00:32 +0700

    add weather model

--- forum/api/models.py
+++ forum/api/models.py
@@ -6,7 +6,10 @@ from django_admin_geomap import GeoItem
 
 
 class News(models.Model):
-    title = models.CharField(max_length=200, verbose_name="Заголовок")
+    title = models.CharField(
+        max_length=200,
+        verbose_name="Заголовок",
+    )
     image = models.ImageField(
         upload_to=r"news_images/%Y/%m/%d/",
         default=None,
@@ -14,8 +17,14 @@ class News(models.Model):
         null=True,
         verbose_name="Главное изображение",
     )
-    text = models.TextField(max_length=500, verbose_name="Текст новости")
-    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
+    text = models.TextField(
+        max_length=500,
+        verbose_name="Текст новости",
+    )
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name="Дата создания",
+    )
     author = models.ForeignKey(
         User,
         blank=True,
@@ -48,7 +57,10 @@ class Place(models.Model, GeoItem):
         default=0,
         verbose_name="Рейтинг",
     )
-    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name="Дата создания",
+    )
 
     class Meta:
         ordering = ("-created_at",)
@@ -60,3 +72,39 @@ class Place(models.Model, GeoItem):
     @property
     def geomap_latitude(self):
         return str(self.lat)
+
+
+class Weather(models.Model):
+    temperature = models.FloatField(
+        verbose_name="Температура",
+    )
+    humidity = models.FloatField(
+        validators=[
+            MinValueValidator(0),
+            MaxValueValidator(100),
+        ],
+        verbose_name="Влажность, %",
+    )
+    pressure = models.PositiveIntegerField(
+        validators=[
+            MinValueValidator(600),
+            MaxValueValidator(900),
+        ],
+        verbose_name="Давление воздуха, мм рт.ст.",
+    )
+    wind_direction = models.PositiveIntegerField(
+        verbose_name="Направление ветра",
+    )
+    wind_speed = models.FloatField(
+        verbose_name="Скорость ветра, м/с",
+    )
+    place = models.ForeignKey(
+        Place,
+        on_delete=models.CASCADE,
+        related_name="weather_data",
+        verbose_name="Место",
+    )
+    date = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name="Дата",
+    )
