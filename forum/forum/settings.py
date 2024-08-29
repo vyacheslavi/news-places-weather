fb97d2302610dd2e42fd9c49916d48a796f9c917
vyacheslavi <vyach.karepov@mail.ru>
2024-08-29 12:59:04 +0700

    add allow hosts

--- forum/forum/settings.py
+++ forum/forum/settings.py
@@ -26,9 +26,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("DJANGO_SECRET")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DJANGO_DEBUG')
+DEBUG = os.environ.get("DJANGO_DEBUG")
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
