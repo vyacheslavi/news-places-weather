e2861f0ee485238e08edccf64ce8c233f85f57e3
vyacheslavi <vyach.karepov@mail.ru>
2024-09-04 19:35:43 +0700

    add CELERY_BROKER_URL setting

--- forum/forum/settings.py
+++ forum/forum/settings.py
@@ -194,7 +194,7 @@ CELERY_TASK_TRACK_STARTED = True
 CELERY_TASK_TIME_LIMIT = 30 * 60
 CELERY_RESULT_BACKEND = "django-db"
 CELERY_RESULT_EXTENDED = True
-CELERY_BROKER_URL = "redis://localhost:6379/0"
+CELERY_BROKER_URL = os.environ.get("CELERY_BROKER_URL")
 CELERY_BEAT_SCHEDULER = "django_celery_beat.schedulers:DatabaseScheduler"
 
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
