9644fef9aa90ce5f017dfe1508e2347407596793
vyacheslavi <vyach.karepov@mail.ru>
2024-09-05 09:43:46 +0700

    add staticfiles url

--- forum/forum/urls.py
+++ forum/forum/urls.py
@@ -21,8 +21,12 @@ from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
 
-urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("summernote/", include("django_summernote.urls")),
-    path("api/v1/", include("api.urls")),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns = (
+    [
+        path("admin/", admin.site.urls),
+        path("summernote/", include("django_summernote.urls")),
+        path("api/v1/", include("api.urls")),
+    ]
+    + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+)
